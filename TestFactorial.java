07fff78a9a2a796e540c01cdd46eeadd43c46339
saisri111 <saisribalabhadra@gmail.com>
2026-08-08 14:16:20 +0530

    My First Commit

---
+++ TestFactorial.java
@@ -0,0 +1,9 @@
+public class TestFactorial
+{
+    public static void main(String[] args)
+    {
+        int n=5;
+        int res=Factorial.calculate(n);
+        System.out.println("Factorial of "+n+"="+result);
+    }
+}
